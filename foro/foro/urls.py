ca1b41200addbd8a43ca667ec73ba2da7731fb2f
JuanpiCasti <juanpablocastiglione01@gmail.com>
2022-11-16 15:54:42 -0300

    vista de prueba

--- foro/foro/urls.py
+++ foro/foro/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import prueba
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('prueba', prueba)
 ]
---
+++ foro/foro/views.py
@@ -0,0 +1,3 @@
+from django.http import HttpResponse
+def prueba(request):
+    return HttpResponse("Hola")
