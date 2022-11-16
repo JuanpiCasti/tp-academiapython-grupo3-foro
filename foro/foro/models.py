8655c304f5840079cafabf2015beceb8510c553c
JuanpiCasti <juanpablocastiglione01@gmail.com>
2022-11-16 16:54:58 -0300

    all_articulos_categoria

--- foro/foro/models.py
+++ foro/foro/models.py
@@ -20,7 +20,7 @@ connection = pymysql.connect(
 def all_articulos():
 	with connection:
 		with connection.cursor() as cursor:
-			sql='SELECT * FROM articulos'
+			sql='SELECT * FROM articulo'
 			cursor.execute(sql)
 		connection.commit()
 		return cursor.fetchall()
@@ -29,10 +29,22 @@ def all_comentarios_de_articulo(id_articulo):
 	with connection:
 		with connection.cursor() as cursor:
 			try:
-				sql=f'SELECT * FROM comentarios WHERE Articulo_idArticulo={id_articulo}'
+				sql=f'SELECT * FROM comentario WHERE Articulo_idArticulo={id_articulo}'
 				cursor.execute(sql)
 			except Exception:
-				print("No se encontro comentarios para un articulo con ese ID.")
+				print("No se encontro comentario para un articulo con ese ID.")
 		connection.commit()
 		return cursor.fetchall()
-		
+
+def all_articulos_categoria(id_categoria):
+		with connection:
+		with connection.cursor() as cursor:
+			try:
+				sql=f"""SELECT * FROM articulo 
+						JOIN articulo_x_categoria as axc ON axc.idArticulo_x_Categoria = articulo.Articulo_idArticulo 
+						JOIN categoria ON axc.Categoria_idCategoria = categoria.idCategoria"""
+				cursor.execute(sql)
+			except Exception:
+				print("No se encontro articulos para una categoria con ese ID.")
+		connection.commit()
+		return cursor.fetchall()
