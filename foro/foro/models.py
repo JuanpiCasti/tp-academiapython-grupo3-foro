84ae028a383a3451e47fbd32129a46e2f38885a1
JuanpiCasti <juanpablocastiglione01@gmail.com>
2022-11-16 16:41:16 -0300

    avances

--- foro/foro/models.py
+++ foro/foro/models.py
@@ -1,16 +1,35 @@
 import pymysql
 
 connection = pymysql.connect(
-			host = '',
-			user = '',
-			password = '',
-			db = ''
+			host = 'localhost:3306',
+			user = 'root',
+			password = '12345678',
+			db = 'foro'
 		)
 
 # def ejemplo():
-#     with connection.cursor() as cursor:
-#         sql = "Aca va la query"
-#         cursor.execute(sql)
-    
-#     connection.commit() # Aplica los cambios
-#     return cursor.fetchall() # Devuelve todo lo encontrado
+#     with connection:
+    #     with connection.cursor() as cursor:
+    #         sql = "Aca va la query"
+    #         cursor.execute(sql)
+        
+    #     connection.commit() # Aplica los cambios si aplica
+    #     return cursor.fetchall() # Devuelve todo lo encontrado si aplica
+
+#Metodos
+def all_articulos():
+	with connection:
+		with connection.cursor() as cursor:
+			sql='SELECT * FROM articulos'
+			cursor.execute(sql)
+		connection.commit()
+		return cursor.fetchall()
+
+def all_comentarios():
+	with connection:
+		with connection.cursor() as cursor:
+			sql='SELECT * FROM comentarios'
+			cursor.execute(sql)
+		connection.commit()
+		return cursor.fetchall()
+		
