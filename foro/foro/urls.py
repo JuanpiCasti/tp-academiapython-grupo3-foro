fbffdcacbdcd07cfb71a72ca778d48aa9fb57103
JuanpiCasti <juanpablocastiglione01@gmail.com>
2022-12-02 17:03:45 -0300

    inicio formulario articulo

--- foro/foro/urls.py
+++ foro/foro/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path('registerUser/', register),
     path('registerUser/register/',registerUser),
     path('editararticulo/<int:article_id>', edit_article),
-    path('actualizararticulo/', update_article)
+    path('actualizararticulo/', update_article),
+    # path('confirmarborrararticulo/<int:raticle_id>', confirm_article_delete)
 ]
